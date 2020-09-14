3a32835ee81b294ad71913b2d514c9eb5afe2ef2
ili <ili@iex.su>
2020-09-14 11:29:47 +0300

    Swiper

---
+++ swiper/init.js
@@ -0,0 +1,24 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    effect: 'fade',
+    autoplay: {
+        delay: 3000, // время задержки между переключениями (время показа)
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
