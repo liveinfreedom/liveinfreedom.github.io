e107c76eb3598a8f854e21b203c686005e6fb9fb
ili <ili@iex.su>
2020-10-07 01:33:09 +0300

    easings

--- easings/assets/scripts.js
+++ easings/assets/scripts.js
@@ -5,11 +5,20 @@ $(function(){
         $input = $('input'),
         duration = 0,
         timeStart = 0,
+        timeElapsed = 0,
+        progress = 0,
+        xMax = 100,
         x = 0;
 
+    function easeInOutSine(progress) {
+        return -(Math.cos(Math.PI * p) - 1) / 2;
+    }
+
     function animate() {
-        if (Date.now() - timeStart < duration) {
-            x += 2;
+       timeElapsed = Date.now() - timeStart;
+        if (timeElapsed < duration) {
+            progress = timeElapsed / duration;
+            x = xMax * easeInOutSine(progress);
             $block.css('transform', 'translateX('+x+'px)');
             requestAnimationFrame(animate);
         }
@@ -19,6 +28,7 @@ $(function(){
         x = 0;
         duration = parseInt($input.val()) * 1000;
         timeStart = Date.now();
+        $block.css('transform', 'translateX(0)');
         requestAnimationFrame(animate);
     });
 });
