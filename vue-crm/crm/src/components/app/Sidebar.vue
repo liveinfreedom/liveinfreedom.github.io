c032fec0f7812d67b30ed4d9ef18ce7ae07e715a
ili <ili@iex.su>
2020-12-14 08:56:34 +0300

    vue-crm

--- vue-crm/crm/src/components/app/Sidebar.vue
+++ vue-crm/crm/src/components/app/Sidebar.vue
@@ -1,26 +1,31 @@
 <template>
   <ul class="sidenav app-sidenav open">
-    <li>
-      <a href="#" class="waves-effect waves-orange pointer">Счет</a>
-    </li>
-    <li>
-      <a href="#" class="waves-effect waves-orange pointer">История</a>
-    </li>
-    <li>
-      <a href="#" class="waves-effect waves-orange pointer">Планирование</a>
-    </li>
-    <li>
-      <a href="#" class="waves-effect waves-orange pointer">Новая запись</a>
-    </li>
-    <li>
-      <a href="#" class="waves-effect waves-orange pointer">Категории</a>
-    </li>
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      tag="li"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <!-- Атрибут href подменяется компонентом router-link без доп.настроек -->
+      <a href="#" class="waves-effect waves-orange pointer">{{ link.title }}</a>
+    </router-link>
   </ul>
 </template>
 
 <script>
 export default {
-  name: "Sidebar"
+  name: "Sidebar",
+  data: () => ({
+    links: [
+      {title: 'Счет', url: '/', exact: true}, // т.к. "/" входит в состав любого урла и без exact этот урл будет всегда активен
+      {title: 'История', url: '/history'},
+      {title: 'Планирование', url: '/planning'},
+      {title: 'Новая запись', url: '/record'},
+      {title: 'Категории', url: '/categories'},
+    ]
+  })
 }
 </script>
 
