08d629607316d20e3516f25e376a316b76871752
ili <ili@iex.su>
2021-02-06 15:10:36 +0300

    vue3-tests

--- vue-js/v3-tests/assets/app.js
+++ vue-js/v3-tests/assets/app.js
@@ -37,3 +37,36 @@ const ListRendering = {
     }
 }
 Vue.createApp(ListRendering).mount('#list-rendering')
+
+// 3. DynamicItems
+const dynamicItems = {
+    data() {
+        return {
+            tasks: [
+                { id: 1, text: 'Learn JavaScript' },
+                { id: 2, text: 'Learn Vue' },
+                { id: 3, text: 'Build something awesome' }
+            ]
+        }
+    }
+}
+const dynamicItemsApp = Vue.createApp(dynamicItems)
+
+dynamicItemsApp.component('alert-item', {
+    props: ['alert'],
+    template:
+        `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+            {{ alert.text }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close" @click="remove">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>`,
+    methods: {
+        remove(){
+            console.log('dynamicItemsRoot.tasks[0]', dynamicItemsRoot.tasks[0])
+            console.log('this.alert', this.alert)
+        }
+    }
+})
+
+const dynamicItemsRoot = dynamicItemsApp.mount('#dynamic-items')
