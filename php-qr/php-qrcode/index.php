4ab1a6a08f86909e35b339a9d4ef1b93254634eb
ili <ili@iex.su>
2024-11-06 12:47:36 +0300

    php-qrcode

--- php-qr/php-qrcode/index.php
+++ php-qr/php-qrcode/index.php
@@ -1,1 +1,31 @@
 <?php
+use chillerlan\QRCode\{QRCode, QROptions};
+use chillerlan\QRCode\Common\EccLevel;
+use chillerlan\QRCode\Output\QROutputInterface;
+
+require './vendor/autoload.php';
+
+$dataSrc = 'otpauth://totp/test?secret=B3JX4VCVJDVNXNZ5&issuer=chillerlan.net';
+$dataDecoded = '';
+$imgPath = './qr.png';
+
+$options = new QROptions([
+    'version'    => 5,
+    'outputType' => QROutputInterface::GDIMAGE_PNG,
+    'eccLevel'   => EccLevel::M,
+]);
+
+$qr = new QRCode($options);
+$qr->render($dataSrc, $imgPath);
+
+try{
+    $dataDecoded = (string)$qr->readFromFile($imgPath);
+}
+catch(Throwable $e){
+    $dataDecoded = 'Ошибка: '.$e->getMessage();
+}
+
+?>
+<img src="<?= $imgPath ?>" alt="QR Code" />
+<br>
+Данные в QR-коде: <?= $dataDecoded ?>
